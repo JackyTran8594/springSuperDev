d2292dad71aa7eb40b651fc38306923cbc14aa17
jackyTran <bachtuvu.dev@gmail.com>
2018-12-26 23:09:50 +0700

    2-2612218

--- webapp/src/main/java/com/wsplanning/webapp/utils/ColorPicker.java
+++ webapp/src/main/java/com/wsplanning/webapp/utils/ColorPicker.java
@@ -1,5 +1,7 @@
 package com.wsplanning.webapp.utils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,7 +11,9 @@ import java.nio.file.Paths;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 
+import groovyjarjarcommonscli.ParseException;
 
 public class ColorPicker {
 
@@ -17,16 +21,27 @@ public class ColorPicker {
 
         Path file = Paths.get("src/main/resources/static/colorPickerData.json");
         JsonArray objArray = new JsonArray();
-       
+
         if (file == null) {
             objArray.add(obj.toString());
             FileWriter jsonfile = new FileWriter(file.toString());
             jsonfile.write(objArray.toString());
             return objArray;
         } else {
-            // JsonParser parser = new JsonParser();
-            // Object object = parser.parse(new FileReader("src/main/resources/static/colorPickerData.json"));
-            objArray.add(obj.toString());
+
+            JsonParser parser = new JsonParser();
+            Object object = parser.parse(new FileReader(file.toString()));
+            JsonObject jsonObject = (JsonObject) object;
+            // jsonObject.
+            objArray = jsonObject.getAsJsonArray();
+            objArray.forEach(item -> {
+                
+                if (item.getAsJsonObject().get("username").toString() == obj.username) {
+                    // item.getAsJsonObject().get("colorCode").
+                }
+            });
+            // objArray.
+            // objArray.add(obj.toString());
             return objArray;
         }
 
