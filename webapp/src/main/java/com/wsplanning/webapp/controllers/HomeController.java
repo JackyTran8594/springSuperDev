4005eb323a220ef32dea9db716169a5681f3030b
TRANTUANVU <vutran165@gmail.com>
2018-12-28 17:14:54 +0700

    1-28122018

--- webapp/src/main/java/com/wsplanning/webapp/controllers/HomeController.java
+++ webapp/src/main/java/com/wsplanning/webapp/controllers/HomeController.java
@@ -4,15 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.wsplanning.webapp.utils.ColorPicker;;
+
 /**
  * Created by ThuyetLV
  */
 @Controller
 public class HomeController {
 
+    public HomeController() {
+        
+    }
+
+    private ColorPicker colorpicker = new ColorPicker();
+
     @GetMapping("/")
     public String index(Model model) {
-        // jsonFileColorThemes();
+        // colorpicker.jsonFileColorThemes(obj);
         return "redirect:/mechanic";
     }
 
